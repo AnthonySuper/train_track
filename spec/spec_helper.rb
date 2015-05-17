4c2c236b73c02b24685ca627c4581e83f8244d16
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-05-17 13:50:54 -0600

    Add codeclimate test integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'train_track'
-
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 ##
 # Dummy model to use for testing
